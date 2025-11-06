9df444fda756bd9d45445ba31176ea869db3de35
RobertoModeneziNeto <robertomodenezi@gmail.com>
2025-11-05 21:06:23 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GelatoLab.Models
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+    }
+}
